c9bd1bd7149ae4c30459094ce9e7e56ea0bdc995
lecepin <383810086@qq.com>
2023-11-10 17:29:03 +0800

    feat:

---
+++ README.md
@@ -0,0 +1,8 @@
+### Cookie Samesite 问题
+
+- https://developers.google.com/search/blog/2020/01/get-ready-for-new-samesitenone-secure?hl=zh-cn
+- https://www.chromium.org/updates/same-site/
+
+Chrome 94 版本不再放行，无法再携带 Cookies，也就是 Electron v14 之后的版本无法通过 `webSecurity: false` 来禁用这些问题。
+
+新版本(v27)，通过复写 `onHeadersReceived` 实现。
---
+++ index.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, session } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false,
+    },
+  });
+
+  win.loadURL("https://github.com/lecepin");
+  win.webContents.openDevTools();
+}
+
+function disableSamesiteCookies(filter = ["*://*/*"]) {
+  session.defaultSession.webRequest.onHeadersReceived(
+    { urls: filter },
+    (details, callback) => {
+      const cookies = details?.responseHeaders?.["set-cookie"];
+      const newCookies = [];
+
+      Array.isArray(cookies) &&
+        cookies.map((it) =>
+          newCookies.push(it.split("; ")[0] + "; Secure; SameSite=None")
+        );
+      details.responseHeaders["set-cookie"] = cookies;
+      callback({ cancel: false, responseHeaders: details.responseHeaders });
+    }
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  disableSamesiteCookies();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
